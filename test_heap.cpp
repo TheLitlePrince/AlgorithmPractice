8ed9fa64d7815bf2089eeda3c759486eb9be65cb
TheLitlePrince <yuanzhuo0118@gmail.com>
2018-09-29 19:27:34 +0800

    complete heaptest

--- test_heap.cpp
+++ test_heap.cpp
@@ -10,11 +10,18 @@ int main()
     {
         mh.insert(i);
     }
+    mh.display();
+
+    int p[] = {43, 53, 53, 123, 5, 2, 41, 5, 2, 5, 68, 9, 21, 643, 3342, 42, 444};
+    mh.makeHeap(p, 17);
+
+    mh.heapSort();
+    mh.display();
 
     for (int i = 0; i < 20; ++i)
     {
-        mh.display();
         mh.pop();
+        mh.display();
     }
 
     system("pause");
