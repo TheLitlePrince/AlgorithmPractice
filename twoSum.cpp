90b7d1a386d98cec819689bdccaa3fc7d7d73184
TheLitlePrince <yuanzhuo0118@gmail.com>
2018-10-18 17:51:31 +0800

    twoSum

---
+++ twoSum.cpp
@@ -0,0 +1,27 @@
+#include "radixSort.cpp"
+#include <iostream>
+
+bool twoSum(int p[], int n, int target)
+{
+    int *arr = new int[n];
+    for (int i = 0; i < n; ++i)
+        arr[i] = p[i];
+    radixsort(arr, n);
+
+    int i = 0, j = n - 1;
+    while (i <= j)
+    {
+        int test = arr[i] + arr[j];
+        if (test > target)
+            j--;
+        else if (test < target)
+            i++;
+        else
+            break;
+    }
+
+    if (i == j)
+        return false;
+    else
+        return true;
+}
